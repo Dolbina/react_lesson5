8876299a2f1b278975cb8c59c9b35c2bcfd2eccc
Ann Dolbina <dolbinaann@gmail.com>
2023-05-09 19:31:57 +0300

    add filtr on page Products

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_lesson5">
       <App />
     </BrowserRouter>
   </StrictMode>
